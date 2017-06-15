33a44c13057f4202bc10492963569e39c5003b06
rileywaters <watersriley@gmail.com>
2017-06-14 17:28:12 -0700

    Added preprocessing step

--- main.m
+++ main.m
@@ -1,1 +1,14 @@
 %%Optical Number Recognition System
+close all; clear all; clc;
+
+Itrain = imread('train.png');
+ItrainC = imcomplement(Itrain);
+
+Itest = imread('test.jpg');
+ItestP = rgb2gray(Itest);
+
+ItestP = ItestP > 80;
+SE = strel('line',10, 10);
+
+ItestP = imclose(ItestP, SE);
+ItestP = imopen(ItestP, SE);
