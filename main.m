c1f42e6d816b30505999717052851ff90de91f1d
rileywaters <watersriley@gmail.com>
2017-06-14 18:42:34 -0700

    cleaned up preprocessing

--- main.m
+++ main.m
@@ -1,14 +1,27 @@
-%%Optical Number Recognition System
+%%Vision Proj Riley Waters
 close all; clear all; clc;
+warning('off', 'Images:initSize:adjustingMag');
+%% Training Image - Preprocessing
 
+%take the input training image complement for regionprops
 Itrain = imread('train.png');
 ItrainC = imcomplement(Itrain);
+imshow(ItrainC);
 
+%% Testing Image - Preprocessing
+
+%take in the testing image
 Itest = imread('test.jpg');
 ItestP = rgb2gray(Itest);
 
+%threshold to binarize
 ItestP = ItestP > 80;
-SE = strel('line',10, 10);
 
+%open and close to remove noise
+SE = strel('line',10, 10);
 ItestP = imclose(ItestP, SE);
 ItestP = imopen(ItestP, SE);
+
+%take complement for the regionprops
+ItestC = imcomplement(ItestP);
+imshow(ItestC);
